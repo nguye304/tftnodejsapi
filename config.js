e1c5f05334e7faffd065d2cb74fafdcde0aaab51
Kevin <kevdevemail@gmail.com>
2019-09-05 01:50:16 -0700

    fixed config

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.RDS_HOSTNAME=process.env.hostname||"tftdbinstance.c8hixssuwv9r.us-west-1
 exports.RDS_USERNAME=process.env.username||"nguye304";
 exports.RDS_PASSWORD=process.env.password||"kip742Jeg";
 exports.RDS_DATABASE=process.env.database||"tftdb";
-exports.RDS_PORT=process.env.hostname||"3306";
+exports.RDS_PORT=process.env.port||"3306";
