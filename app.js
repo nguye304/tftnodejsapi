56675955a9f6621f2594ba9ef5f8b804141ef776
Kevin <kevdevemail@gmail.com>
2019-09-05 01:43:12 -0700

    added http server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const app = express()
-
+var http = require('http')
+var https = require('https')
 var ChampsRouter = require('./routes/Champs')
 var ItemsRouter = require('./routes/Items')
 var RecipesRouter = require('./routes/Recipes')
@@ -11,5 +12,9 @@ app.use('/api/champs',ChampsRouter)
 app.use('/api/items',ItemsRouter)
 app.use('/api/recipes',RecipesRouter)
 
+var httpServer = http.createServer(app);
+var httpsServer= https.createServer(app);
 
+httpServer.listen(8080);
+httpsServer.listen(8443);
 module.exports = app;
