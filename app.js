91bb2ce38e7a083b8fd2f348190418a0bf5e1853
Kevin <kevdevemail@gmail.com>
2019-09-05 01:53:20 -0700

    fixed app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require ('express')
 const app = express()
-var http = require('http')
-var https = require('https')
+
 var ChampsRouter = require('./routes/Champs')
 var ItemsRouter = require('./routes/Items')
 var RecipesRouter = require('./routes/Recipes')
@@ -12,9 +11,6 @@ app.use('/api/champs',ChampsRouter)
 app.use('/api/items',ItemsRouter)
 app.use('/api/recipes',RecipesRouter)
 
-var httpServer = http.createServer(app);
-var httpsServer= https.createServer(app);
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+
 module.exports = app;
